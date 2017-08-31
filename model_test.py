e73e33fd2600e513336d1951d4fdb619e18bd20b
CoopLo <lorsung2@illinois.edu>
2017-08-31 14:33:07 -0500

    changed from hmmsvi to hmmsgd_metaobs because it appears to implement the algorithm from the paper

--- model_test.py
+++ model_test.py
@@ -4,7 +4,7 @@ from numpy import random as rand
 from numpy.random import normal
 import pandas as pd
 import helper_funcs as hf
-from pysvihmm import hmmsvi
+from pysvihmm import hmmsvi, hmmsgd_metaobs
 from pybasicbayes.distributions import gaussian
 import matplotlib
 from matplotlib import  pyplot as plt
@@ -19,8 +19,9 @@ stock_symbols = hf.get_stock_symbols(stock_data)
 single_stock = hf.stock_data_in_one_line(stock_data, stock_symbols, index)
 
 # break up stock data into train and test sets
-train_size = int(0.1*single_stock.size)
+train_size = int(0.2*single_stock.size)
 train_set = np.asarray(single_stock[:train_size])
+train_data = train_set
 test_set = np.asarray(single_stock[train_size:])
 
 # add extra dimension's data
@@ -28,7 +29,6 @@ train_index = [i for i in range(train_set.size)]
 test_index = [i for i in range(test_set.size)]
 
 # putting two dimensions together into columns
-train_data = np.column_stack([train_index, train_set])
 test_data = np.column_stack([test_index, test_set])
 
 # fitting the model
@@ -44,11 +44,37 @@ sigma = np.var(single_stock)**0.5
                        #normal(loc=mu, scale=sigma, size=5)])
 	
 # pi[0] is initial state distribution
-prior_tran = np.asarray(([1., 0., 0., 0., 0.],
-               [0., 1., 0., 0., 0.],
-               [0., 0., 1., 0., 0.],
-               [0., 0., 0., 1., 0.],
-               [0., 0., 0., 0., 1.]))
+
+prior_tran = np.asarray(([0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0.]))
+state=0
+for i in range(1, train_size):
+    if(train_data[i] < (1.005*train_data[i-1]) and train_data[i] > (0.995*train_data[i-1])):
+        prior_tran[0][state] += 1
+        state = 0
+    elif(train_data[i] >= (1.005*train_data[i-1]) and train_data[i] < (1.02*train_data[i-1])):
+        prior_tran[1][state] += 1
+        state = 1
+    elif(train_data[i] >= (1.02*train_data[i-1])):
+        prior_tran[2][state] += 1
+        state = 2
+    elif(train_data[i] <= (0.995*train_data[i-1]) and train_data[i] > (0.98*train_data[i-1])):
+        prior_tran[3][state] += 1
+        state = 3
+    elif(train_data[i] <= (0.98*train_data[i-1])):
+        prior_tran[4][state] += 1
+        state = 4
+
+for i in range(5):
+    for j in range(5):
+        print("prior_tran["+str(i)+"]["+str(j)+"]: " + str(prior_tran[i][j]))
+        prior_tran[i][j] = float(prior_tran[i][j])/train_size
+
+print(str(prior_tran))
+train_data = np.column_stack([train_index, train_set])
 
 sigma = [[sigma]]
 
@@ -60,26 +86,32 @@ prior_emit = [gaussian.Gaussian(mu=mu, sigma=sigma),
               gaussian.Gaussian(mu=mu, sigma=sigma)]
 
 # do the model
-model = hmmsvi.SVIHMM(prior_init = prior_init,
-                      prior_tran = prior_tran,
-                      prior_emit = prior_emit,
-                      obs = single_stock[:train_size])
+#model = hmmsvi.SVIHMM(prior_init = prior_init,
+                      #prior_tran = prior_tran,
+                      #prior_emit = prior_emit,
+                      #obs = single_stock[:train_size])
+model = hmmsgd_metaobs.VBHMM(obs = single_stock[:train_size],
+                             prior_init = prior_init,
+                             prior_tran = prior_tran,
+                             prior_emit = prior_emit)
 
 print(hasattr(model, "var_tran"))
 # try generating before svi step
-obs_seq = model.generate_obs(single_stock[train_size:].shape[0])
+#obs_seq = model.generate_obs(single_stock[train_size:].shape[0])
 #print(obs_seq[1])
 
 # inference step needs minibatches of data. Make them here.
-minibatches = np.ndarray((int(train_size/10), 10))
+buffer_length = 10
+minibatches = np.ndarray((int(train_size/50), 50))
 #print(train_size)
-for i in range(int(train_size/10)):
-    for j in range(10):
-        minibatches[i][j] = train_set[10*i + j]
+for i in range(int(train_size/50)):
+    for j in range(50):
+        minibatches[i][j] = train_set[50*i + j]
+    #print(str(minibatches[i]))
 
 # inference step
 #print(type(model))
-model.infer(minibatches, maxit=10)
+model.infer(minibatches)
 #print("prior_emit: " + str(model.prior_emit))
 #print("prior_tran: " + str(model.prior_tran))
 #print("prior_init: " + str(model.prior_init))
@@ -88,7 +120,7 @@ model.infer(minibatches, maxit=10)
 #print("var_init: " + str(model.var_init))
 
 # generating observation sequence after svi step
-post_obs_seq = model.generate_obs(single_stock[train_size:].shape[0])
+#post_obs_seq = model.generate_obs(single_stock[train_size:].shape[0])
 #print(post_obs_seq[0])
 #print(post_obs_seq[1])
 
