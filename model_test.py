fb7cacef20b1485eacfca94b275f5f9914322dec
CoopLo <lorsung2@illinois.edu>
2017-10-25 21:36:41 -0500

    Trying fresh install on desktop

--- model_test.py
+++ model_test.py
@@ -12,7 +12,7 @@ from matplotlib import  pyplot as plt
 debug = True
 components = 4
 mixtures = 5
-index = 400
+index = 100
 # read in data
 stock_data = pd.read_csv("../data/mquote201010.csv")
 stock_symbols = hf.get_stock_symbols(stock_data)
@@ -36,6 +36,7 @@ prior_init = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
 # observation parameters
 mu = np.asarray([np.mean(single_stock)])
 sigma = np.var(single_stock)**0.5
+
 #print(sigma.shape)
 #obs_params = np.array([normal(loc=mu, scale=sigma, size=5),
                        #normal(loc=mu, scale=sigma, size=5),
@@ -50,52 +51,93 @@ prior_tran = np.asarray(([0., 0., 0., 0., 0.],
                [0., 0., 0., 0., 0.],
                [0., 0., 0., 0., 0.],
                [0., 0., 0., 0., 0.]))
+
 state=0
+# get prior_tran from data
 for i in range(1, train_size):
-    if(train_data[i] < (1.005*train_data[i-1]) and train_data[i] > (0.995*train_data[i-1])):
+    if(train_data[i] < (1.0025*train_data[i-1]) and train_data[i] > (0.9975*train_data[i-1])):
         prior_tran[0][state] += 1
         state = 0
-    elif(train_data[i] >= (1.005*train_data[i-1]) and train_data[i] < (1.02*train_data[i-1])):
+    elif(train_data[i] >= (1.0025*train_data[i-1]) and train_data[i] < (1.01*train_data[i-1])):
         prior_tran[1][state] += 1
         state = 1
-    elif(train_data[i] >= (1.02*train_data[i-1])):
+    elif(train_data[i] >= (1.01*train_data[i-1])):
         prior_tran[2][state] += 1
         state = 2
-    elif(train_data[i] <= (0.995*train_data[i-1]) and train_data[i] > (0.98*train_data[i-1])):
+    elif(train_data[i] <= (0.9975*train_data[i-1]) and train_data[i] > (0.99*train_data[i-1])):
         prior_tran[3][state] += 1
         state = 3
-    elif(train_data[i] <= (0.98*train_data[i-1])):
+    elif(train_data[i] <= (0.99*train_data[i-1])):
         prior_tran[4][state] += 1
         state = 4
 
+
 for i in range(5):
     for j in range(5):
-        print("prior_tran["+str(i)+"]["+str(j)+"]: " + str(prior_tran[i][j]))
+        #print("prior_tran["+str(i)+"]["+str(j)+"]: " + str(prior_tran[i][j]))
         prior_tran[i][j] = float(prior_tran[i][j])/train_size
 
-print(str(prior_tran))
+#print(str(prior_tran))
 train_data = np.column_stack([train_index, train_set])
 
-sigma = [[sigma]]
+#sigma = [[sigma]]
+#sigma = np.cov(train_data)
+#print(sigma)
 
+# PARAMETERS FOR GAUSSIAN, TAKEN FROM TEST FILE
+kappa_0 = 1
+nu_0 = 4
 # prior emissions are gaussian
-prior_emit = [gaussian.Gaussian(mu=mu, sigma=sigma),
-              gaussian.Gaussian(mu=mu, sigma=sigma),
-              gaussian.Gaussian(mu=mu, sigma=sigma),
-              gaussian.Gaussian(mu=mu, sigma=sigma),
-              gaussian.Gaussian(mu=mu, sigma=sigma)]
+print("here?")
+prior_emit = [gaussian.Gaussian(mu = np.array([0,0,0,0,0]), 
+                                sigma = np.eye(5),
+                                mu_0 = np.zeros(5),
+                                kappa_0 = kappa_0,
+                                nu_0 = nu_0),
+              gaussian.Gaussian(mu = np.array([1,1,1,1,1]),
+                                sigma = np.eye(5),
+                                mu_0 = np.zeros(5),
+                                sigma_0 = np.eye(5),
+                                kappa_0 = kappa_0,
+                                nu_0 = nu_0),
+              gaussian.Gaussian(mu = np.array([2,2,2,2,2]),
+                                sigma = np.eye(5),
+                                mu_0 = np.zeros(5),
+                                kappa_0 = kappa_0,
+                                nu_0 = nu_0),
+              gaussian.Gaussian(mu = np.array([3,3,3,3,3]),
+                                sigma = np.eye(5),
+                                mu_0 = np.zeros(5),
+                                sigma_0 = np.eye(5),
+                                kappa_0 = kappa_0,
+                                nu_0 = nu_0),
+              gaussian.Gaussian(mu = np.array([4,4,4,4,4]),
+                                sigma = np.eye(5),
+                                mu_0 = np.zeros(5),
+                                sigma_0 = np.eye(5),
+                                kappa_0 = kappa_0,
+                                nu_0 = nu_0)]
 
-# do the model
-#model = hmmsvi.SVIHMM(prior_init = prior_init,
-                      #prior_tran = prior_tran,
-                      #prior_emit = prior_emit,
-                      #obs = single_stock[:train_size])
+obs = np.array([prior_emit[int(np.round(4*i/train_set.size))].rvs()[0]
+                for i in range(train_set.size)])
+
+#print(prior_tran)
+mu_0 = np.zeros(5)
+sigma_0 = 0.75 * np.cov(obs.T)
+kappa_0 = 0.01
+nu_0 = 4
+prior_emit = [gaussian.Gaussian(mu_0=mu_0, sigma_0=sigma_0, kappa_0=kappa_0, nu_0=nu_0)
+              for _ in range(5)]
+prior_emit = np.array(prior_emit)
+print("here?")
 model = hmmsgd_metaobs.VBHMM(obs = single_stock[:train_size],
                              prior_init = prior_init,
                              prior_tran = prior_tran,
-                             prior_emit = prior_emit)
-
-print(hasattr(model, "var_tran"))
+                             prior_emit = prior_emit,
+                             mb_sz = 50,
+                             verbose = True)
+print("MAYBE HERE")
+#print(hasattr(model, "var_tran"))
 # try generating before svi step
 #obs_seq = model.generate_obs(single_stock[train_size:].shape[0])
 #print(obs_seq[1])
@@ -110,19 +152,7 @@ for i in range(int(train_size/50)):
     #print(str(minibatches[i]))
 
 # inference step
-#print(type(model))
-model.infer(minibatches)
-#print("prior_emit: " + str(model.prior_emit))
-#print("prior_tran: " + str(model.prior_tran))
-#print("prior_init: " + str(model.prior_init))
-#print("var_emit: " + str(model.var_emit))
-#print("var_tran: " + str(model.var_tran))
-#print("var_init: " + str(model.var_init))
-
-# generating observation sequence after svi step
-#post_obs_seq = model.generate_obs(single_stock[train_size:].shape[0])
-#print(post_obs_seq[0])
-#print(post_obs_seq[1])
+model.infer()
 
 # plotting
 plt.style.use('ggplot')
